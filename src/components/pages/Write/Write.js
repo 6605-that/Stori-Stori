2798acffcafdf12a01d2ac81d9f0b4303f360205
Ahuoyiza <sophieoyiza@gmail.com>
2020-08-12 22:50:54 +0100

    login page

--- src/components/pages/Write/Write.js
+++ src/components/pages/Write/Write.js
@@ -6,78 +6,31 @@ export class Write extends Component {
     e.preventDefault();
     this.props.nextStep();
   };
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
   render() {
-    const { values, handleChange } = this.props;
+    //const { values, handleChange } = this.props;
     return (
       <div>
         <NavLogin />
         <div className="container">
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Title"
-              onChange={handleChange("title")}
-              defaultValue={values.title}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="date"
-              placeholder="Upload Date"
-              onChange={handleChange("date")}
-              defaultValue={values.date}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="time"
-              placeholder="Upload Time"
-              onChange={handleChange("time")}
-              defaultValue={values.time}
-            />
-          </div>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Book Cover</span>
-            </div>
-            <div className="custom-file">
-              <input
-                type="file"
-                className="custom-file-input"
-                id="inputGroupFile01"
-                onChange={handleChange("bookCover")}
-                defaultValue={values.bookCover}
-              />
-              <label className="custom-file-label">Choose file</label>
-            </div>
-          </div>
-          <div className="form-group">
-            <input className="form-control" type="text" placeholder="Chapter" />
-          </div>
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Content</span>
-            </div>
-            <textarea
-              className="form-control"
-              aria-label="With textarea"
-              onChange={handleChange("content")}
-              defaultValue={values.content}
-            ></textarea>
-          </div>
-          <br />
-          <div className="form-group">
-            <button
-              type="submit"
-              className="btn btn-primary btn-lg btn-block"
-              onClick={this.continue}
-            >
-              PREVIEW
-            </button>
-          </div>
+          <h1>write</h1>
+          <button
+            type="submit"
+            className="btn btn-light btn-lg "
+            onClick={this.back}
+          >
+            BACK
+          </button>
+          <button
+            type="submit"
+            className="btn btn-primary btn-lg "
+            onClick={this.continue}
+          >
+            SAVE & UPLOAD
+          </button>
         </div>
       </div>
     );
